88d6f2998dd92afc60d1bf7b03c7d695f6ca3e65
Maryam Chertmanova <mchertmanova@gmail.com>
2021-01-26 20:07:43 -0500

    B21 is learning Git

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("B21 we will make it ,future SDET's");
     }
 }
